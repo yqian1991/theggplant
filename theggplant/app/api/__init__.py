039d8200b2a5a40104ccd53950971b717012f2b1
Da Kuang <dak@surveymonkey.com>
2015-12-15 23:06:52 -0500

    include theme api module

--- theggplant/app/api/__init__.py
+++ theggplant/app/api/__init__.py
@@ -3,3 +3,5 @@ def includeme(config):
 	config.include("theggplant.app.api.users")
 	config.include("theggplant.app.api.kitchens")
 	config.include("theggplant.app.api.menuitems")
+	config.include("theggplant.app.api.themes")
+
